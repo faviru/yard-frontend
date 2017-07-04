85655356e4e6949955fc905b3722e769c138a36c
ATKCG\Novitskaya <katerina0708@gmail.com>
2017-07-04 10:16:18 +0300

    convert js script to yml

--- deploy.js
+++
@@ -1,9 +0,0 @@
-const childProcess = require('child_process');
-
-childProcess.execFile('node_modules/.bin/now', ['deploy'], (error, stdout) => {
-  if (error) {
-    throw error;
-  }
-
-  console.log(stdout);
-});
